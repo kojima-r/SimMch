925b769c60cd31b7e2fbf061f21545e1ec575fba
kojima <kojima@cyb.mei.titech.ac.jp>
2016-01-26 11:47:40 +0900

    fix url

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
 mkdir sample
-wget http://www.hark.jp/wiki.cgi?page=SupportedHardware&file=microcone%5Frectf%2Ezip&action=ATTACH sample/
-wget http://www.hark.jp/wiki.cgi?page=SupportedHardware&file=microcone%5Fgeotf%2Ezip&action=ATTACH sample/
+wget "http://www.hark.jp/wiki.cgi?page=SupportedHardware&file=microcone%5Frectf%2Ezip&action=ATTACH" -O sample/microcone_rectf.zip
+wget "http://www.hark.jp/wiki.cgi?page=SupportedHardware&file=microcone%5Fgeotf%2Ezip&action=ATTACH" -O sample/microcone_geotf.zip
 git clone https://github.com/naegawa/HARK_TF_Parser.git
